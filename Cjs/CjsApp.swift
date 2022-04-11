e0eae3ccebb9f6a7316a7dc91424634670810bf3
b-l-u-e <winnie.gitau282@gmail.com>
2022-04-11 07:34:23 +0300

    Initial Commit

---
+++ Cjs/CjsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CjsApp.swift
+//  Cjs
+//
+//  Created by Ahmed Yussufu on 2022-04-11.
+//
+
+import SwiftUI
+
+@main
+struct CjsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cjs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cjs
+//
+//  Created by Ahmed Yussufu on 2022-04-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
